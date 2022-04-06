7cb596d5b13de4a4bdd026e79a5fd9519309dc54
marce <marcel.mtk42@gmail.com>
2022-04-06 14:38:29 +0200

    Creat Options Menu for Scroll View

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.lab5
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
